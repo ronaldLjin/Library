2d976a3e568b9060ee5b85a646cfb57d433e70e5
ronaldLjin <ronyjin123@gmail.com>
2021-10-19 23:22:33 -0600

    Added delete feature and added add book feature.

--- script.js
+++ script.js
@@ -7,13 +7,40 @@ function Book(title,author,pages,read) {
     this.read = read
   }
 
-function addBookToLibrary() {
-  
+let bookTable = document.getElementById('booktable')
+
+displayBook = (book) => {
+    let newRow = document.createElement('tr');
+    bookTable.appendChild(newRow);
+    let garbage = document.createElement('i');
+    let titleCell = document.createElement('td');
+    let authorCell = document.createElement('td');
+    let pagesCell = document.createElement('td');
+    let statusCell = document.createElement('td');
+    titleCell.textContent = book.title;
+    authorCell.textContent = book.author;
+    pagesCell.textContent = book.pages;
+    statusCell.textContent = book.read;
+    garbage.classList.add('fas', 'fa-trash-alt')
+    bookTable.appendChild(garbage);
+    bookTable.appendChild(titleCell);
+    bookTable.appendChild(authorCell);
+    bookTable.appendChild(pagesCell);
+    bookTable.appendChild(statusCell);
+    garbage.addEventListener('click', () => {
+      bookTable.removeChild(newRow);
+      bookTable.removeChild(garbage);
+      bookTable.removeChild(titleCell);
+      bookTable.removeChild(authorCell);
+      bookTable.removeChild(pagesCell);
+      bookTable.removeChild(statusCell);
+    })
 }
 
 newBook = (e) => {
   let testBook = getFormInfo();
   myLibrary.push(testBook);
+  displayBook(testBook);
   e.preventDefault();
 }
 
