b040575e356de989076f703753955a1b8e1b79e0
ronaldLjin <ronyjin123@gmail.com>
2021-10-20 16:39:46 -0600

    Added "status" feature and add new book popup

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let myLibrary = [];
 
-function Book(title,author,pages,read) {
+function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-  }
+}
 
 let bookTable = document.getElementById('booktable')
 
@@ -20,7 +20,15 @@ displayBook = (book) => {
     titleCell.textContent = book.title;
     authorCell.textContent = book.author;
     pagesCell.textContent = book.pages;
-    statusCell.textContent = book.read;
+    let statusButton = document.createElement('button');
+    if (book.read == true) {
+        statusButton.textContent = "Completed";
+        statusButton.classList.add('completed');
+    } else {
+        statusButton.textContent = "In-progress";
+        statusButton.classList.add('inprogress')
+    }
+    statusCell.appendChild(statusButton)
     garbage.classList.add('fas', 'fa-trash-alt')
     bookTable.appendChild(garbage);
     bookTable.appendChild(titleCell);
@@ -28,30 +36,71 @@ displayBook = (book) => {
     bookTable.appendChild(pagesCell);
     bookTable.appendChild(statusCell);
     garbage.addEventListener('click', () => {
-      bookTable.removeChild(newRow);
-      bookTable.removeChild(garbage);
-      bookTable.removeChild(titleCell);
-      bookTable.removeChild(authorCell);
-      bookTable.removeChild(pagesCell);
-      bookTable.removeChild(statusCell);
+        bookTable.removeChild(newRow);
+        bookTable.removeChild(garbage);
+        bookTable.removeChild(titleCell);
+        bookTable.removeChild(authorCell);
+        bookTable.removeChild(pagesCell);
+        bookTable.removeChild(statusCell);
+    })
+    statusButton.addEventListener('click', () => {
+        if (book.read == true) {
+            statusButton.textContent = "In-progress";
+            statusButton.classList.remove('completed');
+            statusButton.classList.add('inprogress');
+            book.read = false;
+        } else {
+            statusButton.textContent = "Completed";
+            statusButton.classList.remove('inprogress');
+            statusButton.classList.add('completed');
+            book.read = true;
+        }
     })
 }
 
-newBook = (e) => {
-  let testBook = getFormInfo();
-  myLibrary.push(testBook);
-  displayBook(testBook);
-  e.preventDefault();
-}
+let message = document.getElementById('message')
 
-let bookInfo = document.getElementById('bookInfo');
-bookInfo.addEventListener('submit', newBook)
+newBook = (e) => {
+    let testBook = getFormInfo();
+    myLibrary.push(testBook);
+    if (testBook.title.length == 0 || testBook.author.length == 0) {
+        message.textContent = "Please ensure your title and author aren't blank."
+    } else if (isNaN(Number(testBook.pages)) || testBook.pages === "" || Number(testBook.pages) % 1 != 0) {
+        message.textContent = 'Please input a whole number under "pages."'
+    } else {
+        displayBook(testBook);
+        bookInfo.style.display = "none";
+        mask.style.display = "none";
+    }
+    e.preventDefault();
+}
 
 
 function getFormInfo() {
-  const title = document.getElementById('title').value
-  const author = document.getElementById('author').value
-  const pages = document.getElementById('pages').value
-  const read = document.getElementById('status').checked
-  return new Book(title,author,pages,read)
+    const title = document.getElementById('title').value
+    const author = document.getElementById('author').value
+    const pages = document.getElementById('pages').value
+    const read = document.getElementById('status').checked
+    return new Book(title, author, pages, read)
 }
+
+let bookInfo = document.getElementById('bookInfo');
+bookInfo.addEventListener('submit', newBook);
+bookInfo.style.display = "none";
+let mask = document.getElementById('mask')
+mask.style.display = "none";
+
+let formButton = document.getElementById('addbook');
+formButton.addEventListener('click', () => {
+    if (bookInfo.style.display === "none") {
+        bookInfo.style.display = "block"
+        mask.style.display = "block";
+    } else {
+        bookInfo.style.display = "none"
+        mask.style.display = "none";
+    }
+})
+mask.addEventListener('click', () => {
+    bookInfo.style.display = "none"
+    mask.style.display = "none";
+})
