aab3c4cf28eda6d02908db73fc9ec41f88275208
ronaldLjin <ronyjin123@gmail.com>
2021-10-19 20:41:51 -0600

    Added a create new book object feature

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-function book(title,author,pages,read) {
+let myLibrary = [];
+
+function Book(title,author,pages,read) {
     this.title = title
-      this.author = author
-      this.pages = pages
-      this.read = read
-      this.info = () => {
-      return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`
-      }
-    }
-    
-    const newbook = new book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet')
-    console.log(newbook.info())
-    
-    
+    this.author = author
+    this.pages = pages
+    this.read = read
+  }
+
+function addBookToLibrary() {
+  
+}
+
+newBook = (e) => {
+  let testBook = getFormInfo();
+  myLibrary.push(testBook);
+  e.preventDefault();
+}
+
+let bookInfo = document.getElementById('bookInfo');
+bookInfo.addEventListener('submit', newBook)
+
+
+function getFormInfo() {
+  const title = document.getElementById('title').value
+  const author = document.getElementById('author').value
+  const pages = document.getElementById('pages').value
+  const read = document.getElementById('status').checked
+  return new Book(title,author,pages,read)
+}
