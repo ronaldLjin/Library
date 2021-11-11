6d71949e50c6407455d7dad49d45388797ef9a8a
ronaldLjin <ronyjin123@gmail.com>
2021-11-11 16:08:20 -0700

    Used class syntax

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+class Book {
+    constructor(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-
+    }
 }
 
 let bookTable = document.getElementById('booktable')
